000d5701cb3d29d3c4703b2942474289bc5ce73d
GudlaVishal <vishalgudla2@gmail.com>
2026-08-21 09:23:13 +0530

    Time: 0 ms (100%), Space: 21.3 MB (19.38%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
@@ -1,66 +1,64 @@
 class Solution {
-    long long gcd(long long a, long long b) {
-        while (b) {
-            long long t = a % b;
-            a = b;
-            b = t;
-        }
-        return a;
-    }
-
-    long long lcm(long long a, long long b) {
-        return a / gcd(a, b) * b;
-    }
-
 public:
+    using ll = long long;
     long long findKthSmallest(vector<int>& coins, int k) {
-        int n = coins.size();
-
-        vector<pair<long long, int>> subsets;
-
-        for (int mask = 1; mask < (1 << n); ++mask) {
-            long long curLcm = 1;
-            int bits = 0;
-
-            for (int i = 0; i < n; ++i) {
-                if (mask & (1 << i)) {
-                    ++bits;
-                    curLcm = lcm(curLcm, coins[i]);
+        sort(coins.begin(), coins.end());
+        vector<int> new_coins;
+        for (int x : coins) {
+            bool flag = true;
+            for (int y : new_coins) {
+                if (x % y == 0) {
+                    flag = false;
+                    break;
                 }
             }
+            if (flag) {
+                new_coins.push_back(x);
+            }
+        }
+        coins = new_coins;
 
-            subsets.push_back({
-                curLcm,
-                bits & 1 ? 1 : -1
-            });
+        int n = coins.size();
+        int m = (1 << n);
+        vector<int> bit_count(m);
+        vector<ll> lcm(m, 1);
+        ll l = k, r = 1ll * coins[0] * k + 1;
+
+        for (int mask = 1; mask < m; mask++) {
+            int pre_mask = mask & (mask - 1);
+            int i = __builtin_ctz(mask);
+
+            ll tmp = lcm[pre_mask] / gcd(lcm[pre_mask], coins[i]);
+            if (tmp <= r / coins[i]) {
+                lcm[mask] = tmp * coins[i];
+            } else {
+                lcm[mask] = r + 1;
+            }
         }
 
-        auto count = [&](long long x) {
-            long long total = 0;
-
-            for (auto [v, sign] : subsets) {
-                if (v > x)
+        auto get = [&](ll x) -> ll {
+            ll count = 0;
+            for (int mask = 1; mask < m; mask++) {
+                if (lcm[mask] > x) {
                     continue;
-
-                total += sign * (x / v);
+                }
+                if (__builtin_popcount(mask) & 1) {
+                    count += x / lcm[mask];
+                } else {
+                    count -= x / lcm[mask];
+                }
             }
-
-            return total;
+            return count;
         };
 
-        long long left = 1;
-        long long right = 1LL * k * (*min_element(coins.begin(),
-                                                   coins.end()));
-
-        while (left < right) {
-            long long mid = left + (right - left) / 2;
-
-            if (count(mid) >= k)
-                right = mid;
-            else
-                left = mid + 1;
+        while (l < r) {
+            ll x = (l + r) >> 1;
+            if (get(x) >= k) {
+                r = x;
+            } else {
+                l = x + 1;
+            }
         }
-
-        return left;
+        return l;
     }
 };
