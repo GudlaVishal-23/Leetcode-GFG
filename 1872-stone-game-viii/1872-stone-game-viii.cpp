56315a1622f8f7c6be820de7d8b9d6666feb0ffa
GudlaVishal <vishalgudla2@gmail.com>
2026-08-24 12:51:52 +0530

    Time: 59 ms (99.27%), Space: 87.9 MB (79.79%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -1,14 +1,23 @@
+constexpr int N=1e5;
+// dp[i]=max(Alice's score-Bob's score 
+// up to ith stone taken by Alice in 1st turn)
+int dp[N]={0};
 class Solution {
 public:
-    int stoneGameVIII(vector<int>& stones) {
-        int n = stones.size();
-        vector<int> pre;
-        partial_sum(stones.begin(), stones.end(), back_inserter(pre));
-        vector<int> f(n);
-        f[n - 1] = pre[n - 1];
-        for (int i = n - 2; i >= 1; --i) {
-            f[i] = max(f[i + 1], pre[i] - f[i + 1]);
+    static int stoneGameVIII(vector<int>& stones) {
+        const int n=stones.size();
+        partial_sum(stones.begin(), stones.end(), stones.begin());
+        dp[n-1]=stones.back();
+        for(int i=n-2; i>=1; i--){
+            dp[i]=max(dp[i+1], stones[i]-dp[i+1]);
         }
-        return f[1];
+        return dp[1];
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
